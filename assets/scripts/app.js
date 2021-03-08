3f745db757a41d72c841d41030ecac67edaf905b
mjeder <matteder96@gmail.com>
2021-03-08 11:35:16 -0600

    Add sign out

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,4 +6,5 @@ $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
+  $('.sign-out').on('submit', authEvents.onSignOut)
 })
