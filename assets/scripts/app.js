c2105040d0ddbfda43185a710d62ac61af17fbc4
mjeder <matteder96@gmail.com>
2021-03-08 21:46:23 -0600

    Begin create film functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const authEvents = require('./auth/events')
+const filmEvents = require('.auth/events')
 
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('.sign-out').on('submit', authEvents.onSignOut)
+  $('#add-films').on('submit', filmEvents.onAddFilm)
 })
---
+++ assets/scripts/film/events.js
@@ -0,0 +1,8 @@
+const api = require('./api')
+const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields')
+
+const onAddFilm = (event) => {
+  event.preventDefault()
+
+}
